82191b9efbdab20b1b659ef9e74da6e04bf9d6c7
JG03dev <jan.gra.ser.001@gmail.com>
2024-07-08 09:51:51 +0200

    Rules done, glider test

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,13 @@ impl Board {
     fn count_neighbors(&self, x: i32, y: i32) -> usize {
         let mut count = 0;
 
-        for x_idx in [-1, 0, 1] {
-            for y_idx in [-1, 0, 1] {
+        for x_idx in [-1, 0, 1].iter().cloned() {
+            for y_idx in [-1, 0, 1].iter().cloned() {
                 if x + x_idx < 0
                     || x + x_idx >= self.width as i32
                     || y + y_idx < 0
                     || y + y_idx >= self.height as i32
-                    || x_idx == 0 && y_idx == 0
+                    || (x_idx == 0 && y_idx == 0)
                 {
                     continue;
                 }
@@ -51,10 +51,7 @@ impl Board {
         let num_neigh = self.count_neighbors(x, y);
         if self.get(x as usize, y as usize) {
             match num_neigh {
-                0 => false,
-                1 => false,
-                2 => true,
-                3 => true,
+                2 | 3 => true,
                 _ => false,
             }
         } else {
@@ -62,26 +59,47 @@ impl Board {
         }
     }
 
+    fn next_generation(&self) -> Board {
+        let mut new_board = Board::new(self.width, self.height);
+
+        for y in 0..self.height as i32 {
+            for x in 0..self.width as i32 {
+                let alive = self.apply_rules(x, y);
+                new_board.set(x as usize, y as usize, alive);
+            }
+        }
+
+        new_board
+    }
+
     fn print(&self) {
         println!("**************************************************************************");
         for y_idx in 0..self.height {
-        for x_idx in 0..self.width {
+            for x_idx in 0..self.width {
                 if self.get(x_idx, y_idx) {
-                    //print!("\u{25A0}");
                     print!("*");
                 } else {
-                    //print!("\u{25A1}");
                     print!(" ");
                 }
             }
             println!();
         }
+        println!("**************************************************************************");
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut my_board = Board::new(100, 30);
 
-    let my_board = Board::new(50, 50);
-    my_board.print();
+    my_board.set(1, 0, true);
+    my_board.set(2, 1, true);
+    my_board.set(0, 2, true);
+    my_board.set(1, 2, true);
+    my_board.set(2, 2, true);
+
+    for _ in 0..10 {
+        my_board.print();
+        my_board = my_board.next_generation();
+        std::thread::sleep(std::time::Duration::from_millis(500)); // To slow down the output
+    }
 }

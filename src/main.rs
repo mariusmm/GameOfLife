f9cba40391e5d2ac27ce332ed3a5807ef34471f9
Jan Krahl <jan@krahl.dev>
2024-07-09 16:47:25 +0200

    Correcciones

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-
-use rand::Rng;  
+use rand::Rng;
 
 #[derive(Clone)]
 struct Cell {
@@ -74,55 +73,33 @@ impl Board {
         }
     }
 
-    fn print(&self) {
-        println!("************************************************************");
-        for y_idx in 0..self.height {
-        for x_idx in 0..self.width {
-                if self.get(x_idx, y_idx) {
-                    //print!("\u{25A0}");
-                    print!("*");
-                } else {
-                    //print!("\u{25A1}");
-                    print!(" ");
-                }
-            }
-            println!();
-        }
-    }
-
-    fn inicializa(&mut self)
-    {
-        let mut rng = rand::thread_rng();  // Crea un generador de números aleatorios
+    fn inicializa(&mut self) {
+        let mut rng = rand::thread_rng();
         for y_idx in 0..self.height {
             for x_idx in 0..self.width {
-                let random_number: u8 = rng.gen_range(0..=1);  // Genera un número aleatorio entre 0 y 1, inclusive
-                self.set(x_idx,y_idx,random_number != 0);
+                let random_number: u8 = rng.gen_range(0..=1);
+                self.set(x_idx, y_idx, random_number != 0);
             }
         }
     }
-
 }
-
 
 extern crate sdl2;
 
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use std::time::Duration;
 
 pub fn main() {
-
-    
     let mut my_board = Board::new(50, 50);
     my_board.inicializa();
-    my_board.print();
-    
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem
+        .window("Conway's Game of Life - Rusty", 550, 550)
         .position_centered()
         .build()
         .unwrap();
@@ -132,37 +109,44 @@ pub fn main() {
     canvas.set_draw_color(Color::RGB(0, 0, 0));
 
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
     'running: loop {
         canvas.set_draw_color(Color::RGB(255, 255, 255));
         canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
                 _ => {}
             }
         }
 
-        canvas.set_draw_color(Color::RGB(0,0,0));
-        // The rest of the game loop goes here...
+        canvas.set_draw_color(Color::RGB(0, 127, 255));
         let mut other_board = my_board.clone();
 
         for y_idx in 0..50 {
             for x_idx in 0..50 {
-                other_board.set(x_idx, y_idx    , my_board.apply_rules(x_idx as i32, y_idx as i32) as bool);
-                
-                if other_board.get(x_idx,y_idx) == true {
-                    canvas.fill_rect(sdl2::rect::Rect::new((x_idx*16) as i32, (y_idx*16) as i32, 16, 16));
+                other_board.set(
+                    x_idx,
+                    y_idx,
+                    my_board.apply_rules(x_idx as i32, y_idx as i32),
+                );
+
+                if other_board.get(x_idx, y_idx) == true {
+                    let _ = canvas.fill_rect(sdl2::rect::Rect::new(
+                        (x_idx * 11) as i32,
+                        (y_idx * 11) as i32,
+                        11,
+                        11,
+                    ));
                 }
             }
         }
 
-
         my_board = other_board;
-        
+
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 5));
     }
